9ac198c0fd1db15c0c7b041c10e2cfba57d5d361
Martin Cantor <andres.cantor-u@mail.escuelaing.edu.co>
2019-03-06 17:18:29 -0500

    Inicio API

---
+++ src/main/java/com/taskManager/TaskManagerAPI/model/Task.java
@@ -0,0 +1,91 @@
+package com.taskManager.TaskManagerAPI.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Task {
+
+    private int id;
+    private String description;
+    private User responsible;
+    private String status;
+    private Date dueDate;
+
+    public Task() {
+    }
+
+    public Task(String description, User responsible, String status, Date dueDate) {
+        this.description = description;
+        this.responsible = responsible;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(User responsible) {
+        this.responsible = responsible;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", responsible=" + responsible +
+                ", status='" + status + '\'' +
+                ", dueDate=" + dueDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id &&
+                Objects.equals(description, task.description) &&
+                Objects.equals(responsible, task.responsible) &&
+                Objects.equals(status, task.status) &&
+                Objects.equals(dueDate, task.dueDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description, responsible, status, dueDate);
+    }
+}
