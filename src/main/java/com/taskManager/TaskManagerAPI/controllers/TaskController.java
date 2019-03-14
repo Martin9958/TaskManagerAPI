cb6c3cef3e33b180f395773174625d54a0ac356a
Martin Cantor <andres.cantor-u@mail.escuelaing.edu.co>
2019-03-14 14:28:09 -0500

    Inicio Controller User

--- src/main/java/com/taskManager/TaskManagerAPI/controllers/TaskController.java
+++ src/main/java/com/taskManager/TaskManagerAPI/controllers/TaskController.java
@@ -1,4 +1,9 @@
 package com.taskManager.TaskManagerAPI.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Task")
 public class TaskController {
 }
--- src/main/java/com/taskManager/TaskManagerAPI/controllers/UserController.java
+++ src/main/java/com/taskManager/TaskManagerAPI/controllers/UserController.java
@@ -1,4 +1,38 @@
 package com.taskManager.TaskManagerAPI.controllers;
 
+import com.taskManager.TaskManagerAPI.exceptions.UserException;
+import com.taskManager.TaskManagerAPI.model.User;
+import com.taskManager.TaskManagerAPI.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/allUsers")
+    public ResponseEntity<?> getUsersList(){
+        return new ResponseEntity<>(userService.getUsersList(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUserById(@PathVariable String userId){
+        try {
+            return new ResponseEntity<>(userService.getUserById(userId),HttpStatus.OK);
+        } catch (UserException e) {
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, e);
+            return new ResponseEntity<>("Error usuario no encontrado",HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    
 }
